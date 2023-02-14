b7e2b2ae5b9e64ebd1d853680c3efc87c51034c2
donjetaibrahimi <idonjeta8@gmail.com>
2023-02-14 17:07:55 +0100

    second
    seconds

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-fdsau
+function scrolll(){
+  var left=document.getElementByClass('.scroll-img');
+  left.scrolllBy(350,0);
+}
+
+function scrollr(){
+  var right=document.getElementByClass('.scroll-img');
+  right.scrollrBy(-350,0);
+}
