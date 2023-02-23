8c028e4868f438c225549fce73335de9a2e5afc6
Lizaajeti <124713897+Lizaajeti@users.noreply.github.com>
2023-02-23 16:40:28 +0100

    liza.

--- js/service.js
+++ js/service.js
@@ -1,34 +1,32 @@
 var slideIndex = 1;
 
 function showSlides(n){
-	var i;
-	var slides = document.getElementsByClassName("mySlides");
+var i;
+var slides = document.getElementsByClassName("mySlides");
 
-	for(i = 0; i < slides.length; i++){
-		slides[i].style.display = "none";
-	}
+for(i = 0; i < slides.length; i++){
+slides[i].style.display = "none";
+}
 
-<<<<<<< HEAD
-	if(n > slides.length){
-=======
-	if(n >  slides.length){
->>>>>>> 11e2b278a966abd1861d2b6b71319d679440f696
-		slideIndex = 1;
-	}
+if(n > slides.length){
+slideIndex = 1;
+}
 
-	if(n<1){
-		slideIndex = slides.length;
-	}
+if(n<1){
+slideIndex = slides.length;
+}
 
-	slides[slideIndex - 1].style.display = "block";
+slides[slideIndex - 1].style.display = "block";
 
 }
 
 showSlides(1);
 
 function pulseSlides(n){
-	slideIndex +=n;
-	showSlides(slideIndex);
+slideIndex +=n;
+showSlides(slideIndex);
 }
 
 setInterval(pulseSlides, 2000, 1);
+
+
